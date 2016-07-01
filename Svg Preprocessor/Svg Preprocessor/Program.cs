ef72850b5f3f07d4297156238c073bef89263f97
Jack Le <Jack Le>
2016-06-30 21:16:10 -0400

    added scaling capability for the wall generator. started svg preprocessor.

---
+++ Svg Preprocessor/Svg Preprocessor/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Svg_Preprocessor {
+    static class Program {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main() {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new SvgPreprocessor());
+        }
+    }
+}
